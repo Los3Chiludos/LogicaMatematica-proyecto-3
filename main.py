d50eb712bcb7bcc8dac94c4a905f8b8849175835
Joei <her20053@uvg.edu.gt>
2022-10-03 11:49:09 -0600

    implementado parser yacc

--- main.py
+++ main.py
@@ -1,94 +1,89 @@
-from ply import lex
+import ply.lex as lex
 import ply.yacc as yacc
 
+# Definir las variables proporcionales y los operadores logicos como alfabetos:
 tokens = (
-    'PLUS',
-    'MINUS',
-    'TIMES',
-    'DIV',
-    'LPAREN',
-    'RPAREN',
-    'NUMBER',
+    'VAR',
+    'NEG',
+    'AND',
+    'OOR',
+    'IMP',
+    'IMD',
+    'LPR',
+    'RPR'
 )
 
-t_ignore = ' \t'
+# Le necesitamos defnir a lex que son los tokens y como es que se ven:
+t_NEG = r'\~'
+t_AND = r'\^'
+t_OOR = r'\|'
+t_IMP = r'\=>'
+t_IMD = r'\<=>'
+t_LPR = r'\('
+t_RPR = r'\)'
 
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIV = r'/'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
+# Definimos un ignore por si se ubican espacios en blanco:
+
+t_ignore = r' '
+
+# Para valores de variables etc tenemos que crear una funcion para que lex lo reconozca:
 
 
-def t_NUMBER(t):
-    r'[0-9]+'
-    t.value = int(t.value)
+def t_VAR(t):
+    r'[p-zP-Z]'
+    t.type = 'VAR'
     return t
 
-
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
+# Para errores de sintaxis:
 
 
 def t_error(t):
-    print("Invalid Token:", t.value[0])
+    print("Caracter no reconocido '%s'" % t.value[0])
     t.lexer.skip(1)
 
 
+# Construimos el lexer:
 lexer = lex.lex()
 
-precedence = (
-    ('left', 'PLUS', 'MINUS'),
-    ('left', 'TIMES', 'DIV'),
-    ('nonassoc', 'UMINUS')
-)
+# Definimos las posibles gramaticas de nuestras expresiones en una funcion:
 
 
-def p_add(p):
-    'expr : expr PLUS expr'
-    p[0] = p[1] + p[3]
+def p_expression(p):
+    '''expression : expression AND expression
+                  | expression OOR expression
+                  | expression IMP expression
+                  | expression IMD expression
+                  | NEG expression
+                  | LPR expression RPR
+                  | VAR'''
+    for pos in p:
+        if pos is not None:
+            print(pos, end=' ')
+    print()
+    p[0] = (p[2], p[1], p[3]) if len(p) > 2 else p[1]
+
+# Definimos un posible empty:
 
 
-def p_sub(p):
-    'expr : expr MINUS expr'
-    p[0] = p[1] - p[3]
+def p_empty(p):
+    """
+    empty :
+    """
+    p[0] = None
 
 
-def p_expr2uminus(p):
-    'expr : MINUS expr %prec UMINUS'
-    p[0] = - p[2]
-
-
-def p_mult_div(p):
-    '''expr : expr TIMES expr
-            | expr DIV expr'''
-
-    if p[2] == '*':
-        p[0] = p[1] * p[3]
-    else:
-        if p[3] == 0:
-            print("Can't divide by 0")
-            raise ZeroDivisionError('integer division by 0')
-        p[0] = p[1] / p[3]
-
-
-def p_expr2NUM(p):
-    'expr : NUMBER'
-    p[0] = p[1]
-
-
-def p_parens(p):
-    'expr : LPAREN expr RPAREN'
-    p[0] = p[2]
-
-
-def p_error(p):
-    print("Syntax error in input!")
-
-
+# Creamos un parseador:
 parser = yacc.yacc()
 
-res = parser.parse("-4*-(3-5)")  # the input
-print(res)
+# # Probamos el lexer:
+# lexer.input("p^q")
+# while True:
+#     tok = lexer.token()
+#     if not tok:
+#         break
+#     print(tok)
+
+
+# Probamos el parser:
+parser.parse("p^q")
+# parser.parse("((p=>q)^p)")
