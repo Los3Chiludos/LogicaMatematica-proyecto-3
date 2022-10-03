d3d9cdd9e4ff9eb992121280543d2062e5de8708
Joei <her20053@uvg.edu.gt>
2022-10-03 12:43:16 -0600

    Realizar un reconocedor de expresiones del cálculo proposicional reducido

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import ply.lex as lex
 import ply.yacc as yacc
 
+
+diccionario_valores = {'p': 0, 'q': 0}
+
 # Definir las variables proporcionales y los operadores logicos como alfabetos:
 tokens = (
     'VAR',
@@ -45,7 +48,24 @@ def t_error(t):
 # Construimos el lexer:
 lexer = lex.lex()
 
+
+# Definimos la precedencia de los operadores logicos:
+precedence = (
+    ('left', 'OOR'),
+    ('left', 'AND'),
+    ('right', 'NEG'),
+    ('left', 'IMP'),
+    ('left', 'IMD')
+)
+
 # Definimos las posibles gramaticas de nuestras expresiones en una funcion:
+
+
+def p_calc(p):
+    """
+    calc : expression
+    """
+    print("Resultado: " + str(run(p[1])))
 
 
 def p_expression(p):
@@ -56,11 +76,29 @@ def p_expression(p):
                   | NEG expression
                   | LPR expression RPR
                   | VAR'''
-    for pos in p:
-        if pos is not None:
-            print(pos, end=' ')
-    print()
-    p[0] = (p[2], p[1], p[3]) if len(p) > 2 else p[1]
+
+    # print("Llamandolo:")
+    # for pos in p:
+    #     if pos is not None:
+    #         print(pos, end=' ')
+    # print()
+
+    # p[0] = (p[2], p[1], p[3]) if len(p) > 2 else p[1]
+
+    # print(len(p))
+    if len(p) > 3:
+        # print("Entro con 2")
+        p[0] = (p[2], p[1], p[3])
+    elif len(p) > 2:
+        p[0] = (p[1], p[2])
+    else:
+        p[0] = p[1]
+
+# Definimos la funcion para errores de sintaxis:
+
+
+def p_error(p):
+    print("Error de sintaxis en '%s'" % p.value)
 
 # Definimos un posible empty:
 
@@ -75,6 +113,31 @@ def p_empty(p):
 # Creamos un parseador:
 parser = yacc.yacc()
 
+# Creamos una funcion para correr el parseador:
+
+
+def run(p):
+    # print("Corriendo:")
+    # for pos in p:
+    #     if pos is not None:
+    #         print(pos, end=' ')
+    # print()
+
+    if type(p) == tuple:
+        if p[0] == '^':
+            return run(p[1]) and run(p[2])
+        elif p[0] == '|':
+            return run(p[1]) or run(p[2])
+        elif p[0] == '=>':
+            return not run(p[1]) or run(p[2])
+        elif p[0] == '<=>':
+            return run(p[1]) == run(p[2])
+        elif p[0] == '~':
+            return not run(p[1])
+    else:
+        return diccionario_valores[str(p[0])]
+
+
 # # Probamos el lexer:
 # lexer.input("p^q")
 # while True:
@@ -85,5 +148,6 @@ parser = yacc.yacc()
 
 
 # Probamos el parser:
-parser.parse("p^q")
+# parser.parse("p<=>q")
+parser.parse("~q")
 # parser.parse("((p=>q)^p)")
--- parsetab.py
+++ parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'AND IMD IMP LPR NEG OOR RPR VARexpression : expression AND expression\n                  | expression OOR expression\n                  | expression IMP expression\n                  | expression IMD expression\n                  | NEG expression\n                  | LPR expression RPR\n                  | VAR\n    empty :\n    '
+_lr_signature = 'leftOORleftANDrightNEGleftIMPleftIMDAND IMD IMP LPR NEG OOR RPR VAR\n    calc : expression\n    expression : expression AND expression\n                  | expression OOR expression\n                  | expression IMP expression\n                  | expression IMD expression\n                  | NEG expression\n                  | LPR expression RPR\n                  | VAR\n    empty :\n    '
     
-_lr_action_items = {'NEG':([0,2,3,5,6,7,8,],[2,2,2,2,2,2,2,]),'LPR':([0,2,3,5,6,7,8,],[3,3,3,3,3,3,3,]),'VAR':([0,2,3,5,6,7,8,],[4,4,4,4,4,4,4,]),'$end':([1,4,9,11,12,13,14,15,],[0,-7,-5,-1,-2,-3,-4,-6,]),'AND':([1,4,9,10,11,12,13,14,15,],[5,-7,5,5,5,5,5,5,-6,]),'OOR':([1,4,9,10,11,12,13,14,15,],[6,-7,6,6,6,6,6,6,-6,]),'IMP':([1,4,9,10,11,12,13,14,15,],[7,-7,7,7,7,7,7,7,-6,]),'IMD':([1,4,9,10,11,12,13,14,15,],[8,-7,8,8,8,8,8,8,-6,]),'RPR':([4,9,10,11,12,13,14,15,],[-7,-5,15,-1,-2,-3,-4,-6,]),}
+_lr_action_items = {'NEG':([0,3,4,6,7,8,9,],[3,3,3,3,3,3,3,]),'LPR':([0,3,4,6,7,8,9,],[4,4,4,4,4,4,4,]),'VAR':([0,3,4,6,7,8,9,],[5,5,5,5,5,5,5,]),'$end':([1,2,5,10,12,13,14,15,16,],[0,-1,-8,-6,-2,-3,-4,-5,-7,]),'AND':([2,5,10,11,12,13,14,15,16,],[6,-8,-6,6,-2,6,-4,-5,-7,]),'OOR':([2,5,10,11,12,13,14,15,16,],[7,-8,-6,7,-2,-3,-4,-5,-7,]),'IMP':([2,5,10,11,12,13,14,15,16,],[8,-8,8,8,8,8,-4,-5,-7,]),'IMD':([2,5,10,11,12,13,14,15,16,],[9,-8,9,9,9,9,9,-5,-7,]),'RPR':([5,10,11,12,13,14,15,16,],[-8,-6,16,-2,-3,-4,-5,-7,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -17,7 +17,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'expression':([0,2,3,5,6,7,8,],[1,9,10,11,12,13,14,]),}
+_lr_goto_items = {'calc':([0,],[1,]),'expression':([0,3,4,6,7,8,9,],[2,10,11,12,13,14,15,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -26,13 +26,14 @@ for _k, _v in _lr_goto_items.items():
        _lr_goto[_x][_k] = _y
 del _lr_goto_items
 _lr_productions = [
-  ("S' -> expression","S'",1,None,None,None),
-  ('expression -> expression AND expression','expression',3,'p_expression','main.py',52),
-  ('expression -> expression OOR expression','expression',3,'p_expression','main.py',53),
-  ('expression -> expression IMP expression','expression',3,'p_expression','main.py',54),
-  ('expression -> expression IMD expression','expression',3,'p_expression','main.py',55),
-  ('expression -> NEG expression','expression',2,'p_expression','main.py',56),
-  ('expression -> LPR expression RPR','expression',3,'p_expression','main.py',57),
-  ('expression -> VAR','expression',1,'p_expression','main.py',58),
-  ('empty -> <empty>','empty',0,'p_empty','main.py',66),
+  ("S' -> calc","S'",1,None,None,None),
+  ('calc -> expression','calc',1,'p_calc','main.py',63),
+  ('expression -> expression AND expression','expression',3,'p_expression','main.py',69),
+  ('expression -> expression OOR expression','expression',3,'p_expression','main.py',70),
+  ('expression -> expression IMP expression','expression',3,'p_expression','main.py',71),
+  ('expression -> expression IMD expression','expression',3,'p_expression','main.py',72),
+  ('expression -> NEG expression','expression',2,'p_expression','main.py',73),
+  ('expression -> LPR expression RPR','expression',3,'p_expression','main.py',74),
+  ('expression -> VAR','expression',1,'p_expression','main.py',75),
+  ('empty -> <empty>','empty',0,'p_empty','main.py',93),
 ]
